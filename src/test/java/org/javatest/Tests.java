b9797d0bec3e355c073a062044d78d8a06359769
Oliver Boyle <olib963@users.noreply.github.com>
2019-01-20 16:11:01 +0000

    Move simple tests to same format as string matchers

--- src/test/java/org/javatest/Tests.java
+++ src/test/java/org/javatest/Tests.java
@@ -11,12 +11,12 @@ public class Tests implements TestProvider {
     public Stream<Test> testStream() {
         return Stream.of(
                 test("All tests expected to pass should pass", () -> {
-                    var tests = allTestsFrom(new SimpleTests(), StringMatcherTests.passing());
+                    var tests = allTestsFrom(SimpleTests.passing(), StringMatcherTests.passing());
                     var result = JavaTest.run(tests);
                     return that(result.succeeded);
                 }),
                 test("All tests expected to fail should fail", () -> {
-                    var tests = allTestsFrom(new SimpleFailingTests(), StringMatcherTests.failing());
+                    var tests = allTestsFrom(SimpleTests.failing(), StringMatcherTests.failing());
                     var results = tests.map(t -> JavaTest.run(Stream.of(t)));
                     var passingTests = results.filter(r -> r.succeeded).collect(Collectors.toList());
                     return that(passingTests.isEmpty());
