39c6bf1de2a42e0c69c8f7b6ce41a170c4b9a052
Oliver Boyle <olib963@users.noreply.github.com>
2019-01-22 08:02:21 +0000

    Extract runnable matcher to new class

--- src/main/java/org/javatest/matchers/ExceptionMatchers.java
+++ src/main/java/org/javatest/matchers/ExceptionMatchers.java
@@ -2,16 +2,8 @@ package org.javatest.matchers;
 
 public interface ExceptionMatchers {
 
-    // TODO describe mismatch
     default Matcher<Runnable> willThrowExceptionThat(Matcher<Exception> exceptionMatcher) {
-        return new PredicateMatcher<>(runnable -> {
-            try {
-                runnable.run();
-                return false;
-            } catch (Exception e) {
-                return exceptionMatcher.matches(e).matches; // TODO compose results. Probably needs to be a new class
-            }
-        }, "throw an exception that was expected to " + exceptionMatcher.matches(null).expected);
+        return new ThrowsExceptionMatcher(exceptionMatcher);
     }
 
 //    that(x, hasMessage(y))
---
+++ src/main/java/org/javatest/matchers/ThrowsExceptionMatcher.java
@@ -0,0 +1,22 @@
+package org.javatest.matchers;
+
+public class ThrowsExceptionMatcher implements Matcher<Runnable> {
+    private final Matcher<Exception> thrownMatcher;
+
+    ThrowsExceptionMatcher(Matcher<Exception> thrownMatcher) {
+        this.thrownMatcher = thrownMatcher;
+    }
+
+    @Override
+    public MatchResult matches(Runnable value) {
+        try {
+            value.run();
+            return MatchResult.mismatch("throw an exception but none was thrown"); // TODO extract the expected from the matcher so it can be used here?
+        } catch (Exception e) {
+            var expectedPrefix = "throw an exception that was expected to ";
+            var caughtMatch = thrownMatcher.matches(e);
+            var expected = expectedPrefix + caughtMatch.expected;
+            return caughtMatch.matches ? MatchResult.match(expected): MatchResult.mismatch(expected); // TODO need to show mismatch here.
+        }
+    }
+}
