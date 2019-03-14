7163da1647b5f9f2b50d9eb15bb900b6186f5e47
Oliver Boyle <olib963@users.noreply.github.com>
2019-03-14 19:24:03 +0000

    Add a way to create custom matchers

--- javatest/javatest-matchers/src/main/java/org/javatest/matchers/Matcher.java
+++ javatest/javatest-matchers/src/main/java/org/javatest/matchers/Matcher.java
@@ -1,6 +1,13 @@
 package org.javatest.matchers;
 
+import org.javatest.matchers.internal.PredicateMatcher;
+
+import java.util.function.Predicate;
+
 public interface Matcher<A> {
     MatchResult matches(A value);
     String describeExpected();
+    static <T> Matcher<T> fromFunctions(Predicate<T> predicate, String expected) {
+        return new PredicateMatcher<>(predicate, expected);
+    }
 }
