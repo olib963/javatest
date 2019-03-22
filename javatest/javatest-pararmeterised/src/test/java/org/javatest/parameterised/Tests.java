d43b430441160f9c9539a515257622854eb75cf6
Oliver Boyle <olib963@users.noreply.github.com>
2019-03-22 18:10:35 +0000

    Remove todos

--- javatest/javatest-pararmeterised/src/test/java/org/javatest/parameterised/Tests.java
+++ javatest/javatest-pararmeterised/src/test/java/org/javatest/parameterised/Tests.java
@@ -5,8 +5,6 @@ import org.javatest.JavaTest;
 public class Tests {
 
     public static void main(String... args) {
-        // TODO larger tuple tests.
-        // TODO Tuple10
         var result = JavaTest.run(new ParameterisedTests());
         if (!result.succeeded) {
             throw new RuntimeException("Tests failed!");
