b51c6937d8e02ce2a6db1f4fd3f2b39b2ddc2616
Oliver Boyle <olib963@users.noreply.github.com>
2019-04-22 17:27:00 +0100

    Collect all files we cannot delete in TemporaryDirectory

--- javatest/javatest-fixtures/src/main/java/org/javatest/fixtures/internal/TemporaryDirectory.java
+++ javatest/javatest-fixtures/src/main/java/org/javatest/fixtures/internal/TemporaryDirectory.java
@@ -4,9 +4,13 @@ import org.javatest.fixtures.Fixture;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TemporaryDirectory implements Fixture<File> {
     private final String filePath;
+
     public TemporaryDirectory(String filePath) {
         this.filePath = filePath;
     }
@@ -22,15 +26,31 @@ public class TemporaryDirectory implements Fixture<File> {
 
     @Override
     public void destroy(File fixture) {
-        // TODO change to collect failures
-        if (fixture.isDirectory()) {
-            var files = fixture.listFiles();
-            if (files != null) {
-                Arrays.stream(files).forEach(this::destroy);
+        var failedToDelete = deleteAll(fixture);
+        if (!failedToDelete.isEmpty()) {
+            throw new IllegalStateException("Could not delete files: " +
+                    failedToDelete.stream().collect(Collectors.joining(", ")));
+        }
+    }
+
+    private List<String> deleteAll(File file) {
+        allFilesToDeleteIn(file).forEach(System.out::println);
+        return allFilesToDeleteIn(file).flatMap(f -> {
+            if (f.delete()) {
+                return Stream.empty();
             }
+            return Stream.of(f.getAbsolutePath());
+        }).collect(Collectors.toList());
+    }
+
+    private Stream<File> allFilesToDeleteIn(File topLevel) {
+        var thisFile = Stream.of(topLevel);
+        var files = topLevel.listFiles();
+        if (topLevel.isDirectory() && files != null) {
+            return Stream.concat(
+                    Arrays.stream(files).flatMap(this::allFilesToDeleteIn),
+                    thisFile);
         }
-        if (!fixture.delete()) {
-            throw new IllegalStateException("Could not delete file " + fixture.getAbsolutePath());
-        }
+        return thisFile;
     }
 }
