53b35bcfb5bd40651da07ee18f1943d3604ea8ba
Oliver Boyle <olib963@users.noreply.github.com>
2019-08-22 13:34:08 +0100

    Test documentation changes in CI

--- gendocs.rb
+++ gendocs.rb
@@ -10,12 +10,6 @@ docFiles.each {
     |source, destination|
     puts "Coalescing #{source} into #{destination}"
 
-    doc = Asciidoctor.load_file source, safe: :unsafe, header_only: true
-
-    # NOTE quick and dirty way to get the attributes set or unset by the document header
-    header_attr_names = (doc.instance_variable_get :@attributes_modified).to_a
-    header_attr_names.each {|k| doc.attributes[%(#{k}!)] = '' unless doc.attr? k }
-
     doc = Asciidoctor.load_file source, safe: :unsafe, parse: false
 
     lines = doc.reader.read.gsub(/^include::(?=.*\[\]$)/m, '\\include::')
