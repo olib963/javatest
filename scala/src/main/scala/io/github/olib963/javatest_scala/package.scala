a5db06c07e784b78fa773c1633912ee09bac37a3
Oliver Boyle <olib963@users.noreply.github.com>
2019-09-08 15:46:03 +0100

    Create package object

---
+++ scala/src/main/scala/io/github/olib963/javatest_scala/package.scala
@@ -0,0 +1,3 @@
+package io.github.olib963
+
+package object javatest_scala extends AllJavaTestSyntax
--- scala/src/test/scala/io/github/olib963/javatest_scala/MyTests.scala
+++ scala/src/test/scala/io/github/olib963/javatest_scala/MyTests.scala
@@ -4,7 +4,10 @@ import java.util.concurrent.Executors
 
 import scala.util.Try
 
-object MyTests extends JavaTestSyntax with FixtureSyntax with BenchmarkSyntax {
+object MyTests {
+
+  // The following line would be needed to inherit all syntax if we weren't already in that package
+  //  import io.github.olib963.javatest_scala._
 
   private val executorServiceFixture = destructibleFixture(Try(Executors.newFixedThreadPool(2)))(e => Try(e.shutdown()))
 
