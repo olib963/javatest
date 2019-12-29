a864decd2070ac9d7c3356ae4d53fc8690388609
oliver.boyle <olib963@users.noreply.github.com>
2019-12-29 12:02:29 +0000

    Fail SBT when tests fail

--- scala/sbt-interface/src/main/scala/io/github/olib963/javatest_sbt_interface/JavaTestScalaFramework.scala
+++ scala/sbt-interface/src/main/scala/io/github/olib963/javatest_sbt_interface/JavaTestScalaFramework.scala
@@ -1,10 +1,13 @@
 package io.github.olib963.javatest_sbt_interface
 
-import io.github.olib963.javatest.{JavaTest, TestResults, TestRunner}
+import java.util.Collections
+
+import io.github.olib963.javatest.{JavaTest, TestRunCompletionObserver, TestRunner}
 import io.github.olib963.javatest_scala.{Runners, Suite}
 import sbt.testing.{Event, EventHandler, Fingerprint, Framework, Logger, OptionalThrowable, Runner, Selector, Status, SubclassFingerprint, Task, TaskDef}
 
 import scala.reflect.ClassTag
+import scala.util.Try
 
 class JavaTestScalaFramework extends Framework {
   override def name(): String = "JavaTest-Scala"
@@ -15,7 +18,7 @@ class JavaTestScalaFramework extends Framework {
   )
 
   override def runner(args: Array[String], remoteArgs: Array[String], testClassLoader: ClassLoader): Runner =
-    JTRunner(args, remoteArgs, testClassLoader)
+    JavaTestRunner(args, remoteArgs, testClassLoader)
 }
 
 object JavaTestScalaFramework {
@@ -35,49 +38,53 @@ object JavaTestScalaFramework {
   val RunnerFingerprint = new ScalaFingerPrint[Runners]
 }
 
-// TODO I think the assumption is that you return separate tasks per definition passed rather than defining a run up front.
-// TODO for now the simplest thing I could think of was to just return constants and make the runner invoke JavaTest. Need to revisit to implement integration with events & logging etc.
-case class JTRunner(args: Array[String], remoteArgs: Array[String], testClassLoader: ClassLoader) extends Runner {
+case class JavaTestRunner(args: Array[String], remoteArgs: Array[String], testClassLoader: ClassLoader) extends Runner {
   override def done(): String = ""
 
-  // TODO error handling etc.
-  override def tasks(taskDefs: Array[TaskDef]): Array[Task] = {
-    val testsCases = taskDefs.flatMap { t => t.fingerprint() match {
-      case JavaTestScalaFramework.SuiteFingerprint => Some(Left(moduleOf[Suite](t.fullyQualifiedName())))
-      case JavaTestScalaFramework.RunnerFingerprint => Some(Right(moduleOf[Runners](t.fullyQualifiedName())))
-      case other => None
-    }}
-
-    val singleTestRunner: TestRunner = testsCases.collect{ case Left(suite) => suite}.toSeq
-    val runners = singleTestRunner +: testsCases.collect{ case Right(runner) => runner}.flatMap(_.Runners).toSeq
-    import CollectionConverters.toJava
-    val results = JavaTest.run(toJava(runners))
-    Array(JTTask(results))
-  }
+  override def tasks(taskDefs: Array[TaskDef]): Array[Task] =
+    taskDefs.collect {
+      case t if t.fingerprint() == JavaTestScalaFramework.SuiteFingerprint =>
+        val toRun: () => Seq[TestRunner] = () => List(moduleOf[Suite](t.fullyQualifiedName()))
+        JavaTestTask(toRun, t)
+      case t if t.fingerprint() == JavaTestScalaFramework.RunnerFingerprint =>
+        val toRun = () => moduleOf[Runners](t.fullyQualifiedName()).Runners
+        JavaTestTask(toRun, t)
+    }
 
   private def moduleOf[A](classToLoad: String): A =
     testClassLoader.loadClass(s"$classToLoad$$").getDeclaredField("MODULE$").get(null).asInstanceOf[A]
 }
 
-// This task does not actually seem to be invoked by SBT. TODO Fix the fingerprints and try to return the results for each test?
-case class JTTask(results: TestResults) extends Task {
-  val fakeFingerprint = new Fingerprint {
-  }
+case class JavaTestTask(toRun: () => Seq[TestRunner], taskDef: TaskDef) extends Task {
   override def tags(): Array[String] = Array()
 
+  // TODO better error handling.
+  // TODO more events
+  // TODO how do we use the loggers? are they needed? We could hook them in as observers?
   override def execute(eventHandler: EventHandler, loggers: Array[Logger]): Array[Task] = {
-    if(!results.succeeded){
-      eventHandler.handle(new Event{
-        override def fullyQualifiedName(): String = "Failure"
-        override def fingerprint(): Fingerprint = fakeFingerprint
-        override def selector(): Selector = null
-        override def status(): Status = Status.Failure
-        override def throwable(): OptionalThrowable = new OptionalThrowable()
-        override def duration(): Long = 0
-      })
+    val result = Try(toRun()).map(r => JavaTest.run(CollectionConverters.toJava(r), Collections.emptyList[TestRunCompletionObserver]()))
+
+    result.foreach {
+      results =>
+        if(!results.succeeded){
+          eventHandler.handle(createEvent(Status.Failure))
+        }
+    }
+
+    result.failed.foreach {
+      error =>
+        eventHandler.handle(createEvent(Status.Error, error = Some(error)))
     }
     Array()
   }
 
-  override def taskDef(): TaskDef = new TaskDef("", fakeFingerprint, false, Array())
+  private def createEvent(eventStatus: Status, error: Option[Throwable] = None): Event  = new Event{
+    override def fullyQualifiedName(): String = taskDef.fullyQualifiedName()
+    override def fingerprint(): Fingerprint = taskDef.fingerprint()
+    override def selector(): Selector = taskDef.selectors().head // TODO what if the number of selectors != 1?
+    override def status(): Status = eventStatus
+    override def throwable(): OptionalThrowable = error.fold(new OptionalThrowable())(new OptionalThrowable(_))
+    override def duration(): Long = -1
+  }
+
 }
--- scala/sbt-test/build.sbt
+++ scala/sbt-test/build.sbt
@@ -10,3 +10,10 @@ crossScalaVersions := Seq(scala11, scala12, scala13)
 resolvers += Resolver.sonatypeRepo("snapshots")
 
 javatestScalacheckVersion := Some("1.14.2") // TODO how do we want to configure this? Perhaps an ADT of NoScalaCheck | Defaults | Provided?
+
+scalacOptions ++= (CrossVersion.partialVersion(scalaVersion.value) match {
+  case Some((2, scalaMajor)) if scalaMajor == 11 =>
+    // Scala 11 has trouble with static functions on interfaces. Not sure why we need to set this option but the compiler tells us to.
+    Seq("-target:jvm-1.8")
+  case _ => Seq.empty
+})
